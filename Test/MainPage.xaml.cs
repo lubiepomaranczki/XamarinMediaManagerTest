7eca31e3cc420aedd3238dadde2ad1bbbbf3ed84
Łukasz Ławicki <lla@trackman.com>
2020-05-30 13:22:17 +0200

    Minimum reproducable project

---
+++ Test/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using MediaManager;
+using MediaManager.Library;
+using Xamarin.Forms;
+
+namespace Test
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            //this is what I want to achieve which does not work. On iOS it works
+            //CrossMediaManager.Current.Play(new MediaItem("https://vjs.zencdn.net/v/oceans.mp4"), startAt: TimeSpan.FromMilliseconds(0), stopAt: TimeSpan.FromMilliseconds(2500));
+
+            //this is what does not work either. On iOS it works
+            //CrossMediaManager.Current.Play(new MediaItem("https://vjs.zencdn.net/v/oceans.mp4"));
+        }
+    }
+}
